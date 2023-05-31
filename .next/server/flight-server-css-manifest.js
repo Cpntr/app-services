198e36f860fed3d0e1cef1619072af4cf1b74a67
Chandra Prakash Sharma <cpsharma.ntr@gmail.com>
2023-05-31 22:52:52 +0530

    Next js app for App services REST api

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,18 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "E:\\XAMPP\\htdocs\\Nextjs\\app-services\\src\\app\\layout.tsx": [
+      "E:\\XAMPP\\htdocs\\Nextjs\\app-services\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "E:\\XAMPP\\htdocs\\Nextjs\\app-services\\src\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "E:\\XAMPP\\htdocs\\Nextjs\\app-services\\src\\app\\page": [
+      "E:\\XAMPP\\htdocs\\Nextjs\\app-services\\src\\app\\globals.css",
+      "E:\\XAMPP\\htdocs\\Nextjs\\app-services\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "E:\\XAMPP\\htdocs\\Nextjs\\app-services\\src\\app\\rawdata\\page": [
+      "E:\\XAMPP\\htdocs\\Nextjs\\app-services\\src\\app\\globals.css",
+      "E:\\XAMPP\\htdocs\\Nextjs\\app-services\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
